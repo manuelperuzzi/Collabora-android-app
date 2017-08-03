eefd7fa764eccf012ce5082430761d19ef32b65f
Mattia Oriani <mattiaoriani@gmail.com>
2017-08-03 12:02:44 +0200

    add listview e addnotebutton

--- app/src/main/java/org/gammf/collabora_android/collabora_android_app/MainActivity.java
+++ app/src/main/java/org/gammf/collabora_android/collabora_android_app/MainActivity.java
@@ -1,13 +1,50 @@
 package org.gammf.collabora_android.collabora_android_app;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] itemName = new String[]{
+            "Item1",
+            "Item2",
+            "Item3",
+            "Item4",
+            "Item5",
+            "Item6",
+            "Item7",
+            "Item8",
+            "Item9",
+    };
+
+    ArrayList<String> item = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView =(ListView) findViewById(R.id.listView);
+        for(String s : itemName){
+            item.add(s);
+        }
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item);
+        listView.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btnAddNotes);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Fra poco potrai aggiungere una nuova nota :P", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
