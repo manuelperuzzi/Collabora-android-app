0167298fcb36b481703800ed55adca1906742e3c
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-12 17:56:56 +0200

    Added builder to create a simple user object

--- app/src/main/java/org/gammf/collabora_android/collaborations/SimpleUser.java
+++ app/src/main/java/org/gammf/collabora_android/collaborations/SimpleUser.java
@@ -4,9 +4,6 @@ import org.joda.time.DateTime;
 
 /**
  * @author Manuel Peruzzi
- */
-
-/**
  * Simple class representing a standard user.
  */
 public class SimpleUser implements User {
@@ -17,7 +14,7 @@ public class SimpleUser implements User {
     private final String surname;
     private final DateTime birthday;
 
-    protected SimpleUser(final String username, final String email, final String name,
+    private SimpleUser(final String username, final String email, final String name,
                          final String surname, final DateTime birthday) {
         this.username = username;
         this.email = email;
@@ -37,7 +34,7 @@ public class SimpleUser implements User {
     }
 
     @Override
-    public String getFirstName() {
+    public String getName() {
         return name;
     }
 
@@ -50,5 +47,49 @@ public class SimpleUser implements User {
     public DateTime getBirthday() {
         return birthday;
     }
-    
+
+    /**
+     * @author Manuel Peruzzi
+     * This nested class is used as a builder to create an object of type SimpleUser
+     */
+    public static class Builder {
+
+        private String username;
+        private String email;
+        private String name;
+        private String surname;
+        private DateTime birthday;
+
+        public Builder username(final String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder email(final String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder name(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder surname(final String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder birthday(final DateTime birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public SimpleUser build() {
+            return new SimpleUser(username, email, name, surname, birthday);
+        }
+
+    }
+
 }
+
