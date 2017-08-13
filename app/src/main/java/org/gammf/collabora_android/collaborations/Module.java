b9d151ebc52d3d1d2a4cca9756211768a61c7ab2
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-13 12:54:02 +0200

    Added module interface

---
+++ app/src/main/java/org/gammf/collabora_android/collaborations/Module.java
@@ -0,0 +1,66 @@
+package org.gammf.collabora_android.collaborations;
+
+import org.gammf.collabora_android.notes.Note;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Manuel Peruzzi
+ * Represents a module as a set of notes in a collaboration.
+ */
+public interface Module {
+
+    /**
+     * @return the module identifier.
+     */
+    String getId();
+
+    /**
+     * @return the description of the module.
+     */
+    String getDescription();
+
+    /**
+     * @param description the description of the module.
+     */
+    void setDescription(String description);
+
+    /**
+     * @return a list containing each note of the module.
+     */
+    List<ModuleNote> getAllNotes();
+
+    /**
+     * Returns a note identified by its id.
+     * @param noteId the identifier of the requested note.
+     * @return the requested note.
+     * @throws NoSuchElementException if the noteId does not exist.
+     */
+    ModuleNote getNote(String noteId) throws NoSuchElementException;
+
+    /**
+     * Adds a note to the module. If the note already exist, it will be overwritten.
+     * @param note the note to be added to the module.
+     * @return true if the note is not in the module, false otherwise.
+     */
+    boolean addNote(Note note);
+
+    /**
+     * Removes a note from the module.
+     * @param noteId the identifier of the note to be removed from the module.
+     * @return true if the note was in the module, false otherwise.
+     */
+    boolean removeNote(String noteId);
+
+    /**
+     * @return the definition of the module state.
+     */
+    ModuleStateDefinition getStateDefinition();
+
+    /**
+     * @param stateDefinition the definition of the module state.
+     */
+    void setStateDefinition(ModuleStateDefinition stateDefinition);
+
+}
