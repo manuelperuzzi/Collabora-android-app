0c5835899584385447c268491a2286bab1c01cce
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-13 11:20:45 +0200

    Fix in collaboration's general interface

--- app/src/main/java/org/gammf/collabora_android/collaborations/Collaboration.java
+++ app/src/main/java/org/gammf/collabora_android/collaborations/Collaboration.java
@@ -33,11 +33,11 @@ public interface Collaboration {
 
     /**
      * Returns a member of the collaboration identified by its user id.
-     * @param userId the identifier of the user.
+     * @param username the identifier of the user.
      * @return the requested member.
      * @throws NoSuchElementException if the userId does not exist.
      */
-    CollaborationMember getMember(String userId) throws NoSuchElementException;
+    CollaborationMember getMember(String username) throws NoSuchElementException;
 
     /**
      * Adds a user to the collaboration with the specified access right. If the user already exists, it will be overwritten.
@@ -45,6 +45,13 @@ public interface Collaboration {
      * @return true if the user is not in the collaboration, false otherwise.
      */
     boolean addMember(CollaborationMember member);
+
+    /**
+     * Removes a member from the collaboration.
+     * @param username the identifier of the user to be removed from the collaboration.
+     * @return true if the user was in the collaboration, false otherwise.
+     */
+    boolean removeMember(String username);
 
     /**
      * @return a list containing each note of the collaboration.
@@ -65,5 +72,12 @@ public interface Collaboration {
      * @return true if the note is not in the collaboration, false otherwise.
      */
     boolean addNote(Note note);
+
+    /**
+     * Removes a note from the collaboration.
+     * @param noteId the identifier of the note to be removed from the collaboration.
+     * @return true if the note was in the collaboration, false otherwise.
+     */
+    boolean removeNote(String noteId);
 
 }
