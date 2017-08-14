0e154b676b314d1fa557a4c79d614b178ceac2e0
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-14 22:27:55 +0200

    Addition of some tests for collaboration to json conversion

--- app/src/test/java/org/gammf/collabora_android/utils/CollaborationUtilsTest.java
+++ app/src/test/java/org/gammf/collabora_android/utils/CollaborationUtilsTest.java
@@ -1,6 +1,5 @@
 package org.gammf.collabora_android.utils;
 
-import org.gammf.collabora_android.collaborations.Collaboration;
 import org.gammf.collabora_android.collaborations.ConcreteProject;
 import org.gammf.collabora_android.collaborations.Project;
 import org.gammf.collabora_android.modules.ConcreteModule;
@@ -8,9 +7,15 @@ import org.gammf.collabora_android.modules.Module;
 import org.gammf.collabora_android.notes.Note;
 import org.gammf.collabora_android.notes.SimpleModuleNote;
 import org.gammf.collabora_android.notes.SimpleNoteBuilder;
+import org.gammf.collabora_android.users.AccessRight;
+import org.gammf.collabora_android.users.CollaborationMember;
+import org.gammf.collabora_android.users.SimpleUser;
+import org.joda.time.DateTime;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Member;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +26,7 @@ import static org.junit.Assert.*;
 public class CollaborationUtilsTest {
 
     private Project project;
+    private CollaborationMember member;
     private Note singleNote;
     private Module firstModule;
     private Module secondModule;
@@ -32,6 +38,15 @@ public class CollaborationUtilsTest {
     public void setUp() throws Exception {
         project = new ConcreteProject("myProjectId", "MyProject");
 
+        member = new CollaborationMember(
+                new SimpleUser.Builder()
+                        .username("myUsername")
+                        .email("myEmail@gmail.com")
+                        .name("myName")
+                        .surname("mySurname")
+                        .birthday(new DateTime(1994, 3, 7, 0, 0))
+                        .build(), AccessRight.ADMIN);
+        project.addMember(member);
         singleNote = new SimpleNoteBuilder("SingleNote")
                 .setNoteID("singleNoteId")
                 .buildNote();
@@ -61,8 +76,12 @@ public class CollaborationUtilsTest {
     public void collaborationToJson() throws Exception {
         final JSONObject json = CollaborationUtils.collaborationToJson(project);
         System.out.println("[CollaborationUtilsTest]: " + json);
-        final Collaboration collaboration = CollaborationUtils.jsonToCollaboration(json);
+        final Project collaboration = (Project) CollaborationUtils.jsonToCollaboration(json);
         //assertEquals(project, collaboration);
+        assertEquals(member, collaboration.getMember(member.getUsername()));
+        //assertEquals(firstModule, collaboration.getModule(firstModule.getId()));
+        //assertEquals(singleNote, collaboration.getNote(singleNote.getNoteID()));
+        //assertEquals(thirdNote, collaboration.getNote(thirdNote.getNoteID()));
     }
 
 }
