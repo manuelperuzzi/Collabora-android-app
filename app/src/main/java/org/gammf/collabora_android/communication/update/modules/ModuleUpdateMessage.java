fd24b0b933dea4beeabbef87374931487b84f4e6
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-16 16:12:09 +0200

    Fix in module update message

--- app/src/main/java/org/gammf/collabora_android/communication/update/modules/ModuleUpdateMessage.java
+++ app/src/main/java/org/gammf/collabora_android/communication/update/modules/ModuleUpdateMessage.java
@@ -1,12 +1,13 @@
 package org.gammf.collabora_android.communication.update.modules;
 
+import org.gammf.collabora_android.communication.update.general.UpdateMessage;
 import org.gammf.collabora_android.modules.Module;
 
 /**
  * @author Manuel Peruzzi
  * A simple interface that represents a message containing an update in a module.
  */
-public interface ModuleUpdateMessage {
+public interface ModuleUpdateMessage extends UpdateMessage {
 
     /**
      * @return the updated module.
