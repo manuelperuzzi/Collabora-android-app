a344e29b8f10b90904895bd95448eb23fd63f790
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-16 16:28:09 +0200

    Changes in the module update message implementation to extends the new abstract class

--- app/src/main/java/org/gammf/collabora_android/communication/update/modules/ConcreteModuleUpdateMessage.java
+++ app/src/main/java/org/gammf/collabora_android/communication/update/modules/ConcreteModuleUpdateMessage.java
@@ -1,6 +1,6 @@
 package org.gammf.collabora_android.communication.update.modules;
 
-import org.gammf.collabora_android.communication.common.MessageType;
+import org.gammf.collabora_android.communication.update.general.AbstractUpdateMessage;
 import org.gammf.collabora_android.communication.update.general.UpdateMessageTarget;
 import org.gammf.collabora_android.communication.update.general.UpdateMessageType;
 import org.gammf.collabora_android.modules.Module;
@@ -9,31 +9,13 @@ import org.gammf.collabora_android.modules.Module;
  * @author ManuelPeruzzi
  * A concrete message representing an update in a module.
  */
-public class ConcreteModuleUpdateMessage implements ModuleUpdateMessage {
+public class ConcreteModuleUpdateMessage extends AbstractUpdateMessage implements ModuleUpdateMessage {
 
-    private final String username;
     private final Module module;
-    private final UpdateMessageType updateType;
 
     public ConcreteModuleUpdateMessage(final String username, final Module module, final UpdateMessageType updateType) {
-        this.username = username;
+        super(username, updateType);
         this.module = module;
-        this.updateType = updateType;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public MessageType getMessageType() {
-        return MessageType.UPDATE;
-    }
-
-    @Override
-    public UpdateMessageType getUpdateType() {
-        return updateType;
     }
 
     @Override
