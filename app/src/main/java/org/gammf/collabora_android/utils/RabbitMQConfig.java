f1c4efdec3eb197218f7a820537af0638f3e2310
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-08-25 21:36:17 +0200

    Created a simple class meant to contain all RabbiMQ configuration values

---
+++ app/src/main/java/org/gammf/collabora_android/utils/RabbitMQConfig.java
@@ -0,0 +1,20 @@
+package org.gammf.collabora_android.utils;
+
+/**
+ * @author Alfredo Maffi
+ * Simple class meant to contain all RabbitMQ configuration values
+ */
+
+public class RabbitMQConfig {
+
+    public static final String BROKER_ADDRESS = "192.168.1.125";
+    public static final String NOTIFICATIONS_EXCHANGE_NAME = "notifications";
+    public static final String UPDATES_EXCHANGE_NAME = "updates";
+    public static final String COLLABORATIONS_EXCHANGE_NAME = "collaborations";
+    public static final String NOTIFICATIONS_QUEUE_PREFIX = "notify.";
+    public static final String COLLABORATIONS_QUEUE_PREFIX = "collaborate";
+    public static final String UPDATES_ROUTING_KEY = "";
+
+
+    private RabbitMQConfig() {}
+}
