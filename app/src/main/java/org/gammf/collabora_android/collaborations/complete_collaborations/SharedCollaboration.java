8fb0613491f8ee988f0758e2595c68900489b1a0
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-28 15:52:54 +0200

    New collaboration interface structure with the subdivision between shared collaborations and private collaborations

--- app/src/main/java/org/gammf/collabora_android/collaborations/complete_collaborations/SharedCollaboration.java
+++ app/src/main/java/org/gammf/collabora_android/collaborations/complete_collaborations/SharedCollaboration.java
@@ -1,6 +1,7 @@
 package org.gammf.collabora_android.collaborations.complete_collaborations;
 
 import org.gammf.collabora_android.notes.Note;
+import org.gammf.collabora_android.users.CollaborationMember;
 
 import java.util.NoSuchElementException;
 import java.util.Set;
@@ -25,6 +26,40 @@ public interface Collaboration {
      * @param name the name of the collaboration.
      */
     void setName(String name);
+
+    /**
+     * @return a list containing each member of the collaboration.
+     */
+    Set<CollaborationMember> getAllMembers();
+
+    /**
+     * Checks if the given user is a member of the collaboration.
+     * @param username the identifier of the user.
+     * @return true if the user is a member of the collaboration, false otherwise.
+     */
+    boolean containsMember(String username);
+
+    /**
+     * Returns a member of the collaboration identified by its user id.
+     * @param username the identifier of the user.
+     * @return the requested member.
+     * @throws NoSuchElementException if the userId does not exist.
+     */
+    CollaborationMember getMember(String username) throws NoSuchElementException;
+
+    /**
+     * Adds a user to the collaboration with the specified access right. If the user already exists, it will be overwritten.
+     * @param member the user to be added to the collaboration.
+     * @return true if the user is not in the collaboration, false otherwise.
+     */
+    boolean addMember(CollaborationMember member);
+
+    /**
+     * Removes a member from the collaboration.
+     * @param username the identifier of the user to be removed from the collaboration.
+     * @return true if the user was in the collaboration, false otherwise.
+     */
+    boolean removeMember(String username);
 
     /**
      * @return a list containing each note of the collaboration.
