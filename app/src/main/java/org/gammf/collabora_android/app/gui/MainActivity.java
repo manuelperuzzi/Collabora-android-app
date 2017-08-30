6194452d26f00f09e74d72b758bf2cd4c739693a
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-08-30 19:01:53 +0200

    Partial adjustments in MainActivity and related class (DialogCollaborationFragment) in order to integrate the creation of new collaborations with RabbitMQ services and tasks

--- app/src/main/java/org/gammf/collabora_android/app/gui/MainActivity.java
+++ app/src/main/java/org/gammf/collabora_android/app/gui/MainActivity.java
@@ -1,7 +1,10 @@
 package org.gammf.collabora_android.app.gui;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.net.Uri;
@@ -14,6 +17,7 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -45,11 +49,14 @@ import org.gammf.collabora_android.communication.update.general.UpdateMessageTyp
 import org.gammf.collabora_android.short_collaborations.CollaborationsManager;
 import org.gammf.collabora_android.short_collaborations.ShortCollaboration;
 import org.gammf.collabora_android.users.SimpleCollaborationMember;
+import org.gammf.collabora_android.users.SimpleUser;
 import org.gammf.collabora_android.users.User;
 import org.gammf.collabora_android.utils.AccessRight;
 import org.gammf.collabora_android.utils.CollaborationType;
 import org.gammf.collabora_android.utils.LocalStorageUtils;
+import org.gammf.collabora_android.utils.MandatoryFieldMissingException;
 import org.gammf.collabora_android.utils.MessageUtils;
+import org.joda.time.DateTime;
 import org.json.JSONException;
 
 import java.io.FileNotFoundException;
@@ -81,6 +88,13 @@ public class MainActivity extends AppCompatActivity
     private User user;
     private CollaborationsManager collaborationsManager;
 
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            refreshCollaborationLists();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,7 +102,11 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter("update.collaborations.on.gui"));
+
         try {
+            final User temporaryUser = new SimpleUser.Builder().name("peru").surname("peruperu").username("peru13").birthday(new DateTime(675748765489L)).email("manuel.peruzzi@studio.unibo.it").build();
+            LocalStorageUtils.writeUserToFile(getApplicationContext(), temporaryUser);
             user = LocalStorageUtils.readUserFromFile(getApplicationContext());
         } catch (final FileNotFoundException e) {
             //TODO show login/registration page
@@ -96,15 +114,9 @@ public class MainActivity extends AppCompatActivity
             //TODO ?
         } catch (IOException e) {
             //TODO ?
-        }
-        try {
-            collaborationsManager = LocalStorageUtils.readShortCollaborationsFromFile(getApplicationContext());
-        } catch (final IOException e) {
-            e.printStackTrace();
-        } catch (final JSONException e) {
+        } catch (MandatoryFieldMissingException e) {
             e.printStackTrace();
         }
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -124,37 +136,8 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        final Map<String, List<ShortCollaboration>> expandableListDetail = new HashMap<>();
-        expandableListDetail.put(getResources().getString(R.string.personal_drawer), filterCollaborationsFromManager(CollaborationType.PRIVATE));
-        expandableListDetail.put(getResources().getString(R.string.groups_drawer), filterCollaborationsFromManager(CollaborationType.GROUP));
-        expandableListDetail.put(getResources().getString(R.string.project_drawer), filterCollaborationsFromManager(CollaborationType.PROJECT));
-
-        final List<String> expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
-        expandableListView = (ExpandableListView) findViewById(R.id.expandableListCollaborations);
-        expandableListAdapter = new CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
-        expandableListView.setAdapter(expandableListAdapter);
-        for(int i=0; i < expandableListAdapter.getGroupCount(); i++)
-            expandableListView.expandGroup(i);
-        //In esecuzione quando si clicca su un elemento del menu
-        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v,
-                                        int groupPosition, int childPosition, long id) {
-                final ShortCollaboration collabSelected =
-                        expandableListDetail.get(
-                                expandableListTitle.get(groupPosition)).get(childPosition);
-                selectItem(collabSelected);
-                /*
-                       Per il nome del gruppo: expandableListTitle.get(groupPosition)
-                       Per il nome selezionato:
-                                expandableListDetail.get(
-                                expandableListTitle.get(groupPosition)).get(
-                                childPosition)
-                */
-                return false;
-            }
-        });
-
+        refreshCollaborationLists();
+        
         final Intent notificationIntent = new Intent(getApplicationContext(), NotificationsSubscriberService.class);
         notificationIntent.putExtra("username", user.getUsername());
         notificationIntent.putStringArrayListExtra("collaborationsIds", new ArrayList<>(getExistingCollaborationsIds()));
@@ -164,7 +147,7 @@ public class MainActivity extends AppCompatActivity
         collaborationIntent.putExtra("username", user.getUsername());
         startService(collaborationIntent);
 
-        this.geoManager = new GeofenceManager(this);
+        //this.geoManager = new GeofenceManager(this);
 
 /*
         //simple examples, 2 set and 1 delete to test.
@@ -300,12 +283,12 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onStart() {
         super.onStart();
-        if (!checkPermissions()) {
-            requestPermissions();
-        }
+        //if (!checkPermissions()) {
+        //    requestPermissions();
+        //}
 
-        this.geoManager.addGeofence("id1","contenuto prima posizione",new LatLng(44.261746, 12.338030));
-        this.geoManager.addGeofence("id2","contenuto seconda posizione",new LatLng(44.159825, 12.430086));
+        //this.geoManager.addGeofence("id1","contenuto prima posizione",new LatLng(44.261746, 12.338030));
+        //this.geoManager.addGeofence("id2","contenuto seconda posizione",new LatLng(44.159825, 12.430086));
 
         //this.geoManager.removeGeofence("id2");
     }
@@ -448,6 +431,12 @@ public class MainActivity extends AppCompatActivity
         dialog.dismiss();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
     public void showNoticeDialog() {
         // Create an instance of the dialog fragment and show it
         DialogFragment dialog = DialogNewCollaborationFragment.newInstance();
@@ -463,9 +452,11 @@ public class MainActivity extends AppCompatActivity
 
     private List<ShortCollaboration> filterCollaborationsFromManager(final CollaborationType collaborationType) {
         final List<ShortCollaboration> collaborations = new ArrayList<>();
-        for(final ShortCollaboration collaboration : collaborationsManager.getAllCollaborations()) {
-            if(collaboration.getCollaborationType().equals(collaborationType)) {
-                collaborations.add(collaboration);
+        if(collaborationsManager != null) {
+            for (final ShortCollaboration collaboration : collaborationsManager.getAllCollaborations()) {
+                if (collaboration.getCollaborationType().equals(collaborationType)) {
+                    collaborations.add(collaboration);
+                }
             }
         }
         return collaborations;
@@ -473,10 +464,51 @@ public class MainActivity extends AppCompatActivity
 
     public List<String> getExistingCollaborationsIds() {
         final List<String> collaborationIds = new ArrayList<>();
-        for(final ShortCollaboration collaboration: collaborationsManager.getAllCollaborations()) {
-            collaborationIds.add(collaboration.getId());
+        if(collaborationsManager != null) {
+            for (final ShortCollaboration collaboration : collaborationsManager.getAllCollaborations()) {
+                collaborationIds.add(collaboration.getId());
+            }
         }
         return collaborationIds;
+    }
+
+    public void refreshCollaborationLists() {
+        try {
+            collaborationsManager = LocalStorageUtils.readShortCollaborationsFromFile(getApplicationContext());
+        } catch (final JSONException e) {
+            e.printStackTrace();
+        }
+
+        final Map<String, List<ShortCollaboration>> expandableListDetail = new HashMap<>();
+        expandableListDetail.put(getResources().getString(R.string.personal_drawer), filterCollaborationsFromManager(CollaborationType.PRIVATE));
+        expandableListDetail.put(getResources().getString(R.string.groups_drawer), filterCollaborationsFromManager(CollaborationType.GROUP));
+        expandableListDetail.put(getResources().getString(R.string.project_drawer), filterCollaborationsFromManager(CollaborationType.PROJECT));
+
+        final List<String> expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListCollaborations);
+        expandableListAdapter = new CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        for(int i=0; i < expandableListAdapter.getGroupCount(); i++)
+            expandableListView.expandGroup(i);
+        //In esecuzione quando si clicca su un elemento del menu
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                final ShortCollaboration collabSelected =
+                        expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(childPosition);
+                selectItem(collabSelected);
+                /*
+                       Per il nome del gruppo: expandableListTitle.get(groupPosition)
+                       Per il nome selezionato:
+                                expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition)
+                */
+                return false;
+            }
+        });
     }
 }
 
