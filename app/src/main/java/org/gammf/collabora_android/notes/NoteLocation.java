76fe44116a4bdcb973c97acf8983dc7ae5462785
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-09-01 15:07:17 +0200

    Redefinition of equals and hash code methods

--- app/src/main/java/org/gammf/collabora_android/notes/NoteLocation.java
+++ app/src/main/java/org/gammf/collabora_android/notes/NoteLocation.java
@@ -28,4 +28,23 @@ public class NoteLocation implements Location {
     public Double getLongitude() {
         return this.longitude;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NoteLocation that = (NoteLocation) o;
+
+        return latitude != null ? latitude.equals(that.latitude) : that.latitude == null &&
+                (longitude != null ? longitude.equals(that.longitude) : that.longitude == null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = latitude != null ? latitude.hashCode() : 0;
+        result = 31 * result + (longitude != null ? longitude.hashCode() : 0);
+        return result;
+    }
 }
