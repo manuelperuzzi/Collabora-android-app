808ab9ed1e1cb2bc30b7280e3e71bdcb8dbc80fb
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-09-07 01:22:35 +0200

    Added javadoc

--- app/src/main/java/org/gammf/collabora_android/communication/allCollaborations/ConcreteAllCollaborationsMessage.java
+++ app/src/main/java/org/gammf/collabora_android/communication/allCollaborations/ConcreteAllCollaborationsMessage.java
@@ -7,11 +7,19 @@ import org.gammf.collabora_android.communication.common.MessageType;
 import java.util.Collections;
 import java.util.List;
 
-
+/**
+ * @author Manuel Peruzzi
+ * Concrete class representing a message containing a list of collaborations.
+ */
 public class ConcreteAllCollaborationsMessage extends AbstractMessage implements AllCollaborationsMessage {
 
     private final List<Collaboration> collaborations;
 
+    /**
+     * Class constructor.
+     * @param username tha identifier of the user.
+     * @param collaborations all the collaborations of the user.
+     */
     public ConcreteAllCollaborationsMessage(final String username, final List<Collaboration> collaborations) {
         super(username, MessageType.ALL_COLLABORATIONS);
         this.collaborations = collaborations;
