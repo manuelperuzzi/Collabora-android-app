0e153655cb8ff61a0e10ff729d6baf73dece348b
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-09-11 21:21:59 +0200

    Changes to make firebase notifications not clickable

--- app/src/main/java/org/gammf/collabora_android/app/FirebaseNotificationService.java
+++ app/src/main/java/org/gammf/collabora_android/app/FirebaseNotificationService.java
@@ -1,15 +1,12 @@
 package org.gammf.collabora_android.app;
 
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import org.gammf.collabora_android.app.gui.MainActivity;
 import org.gammf.collabora_android.users.User;
 import org.gammf.collabora_android.utils.LocalStorageUtils;
 import org.json.JSONException;
@@ -34,22 +31,16 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
         }
     }
 
-    private void sendNotification(RemoteMessage remoteMessage) {
-        RemoteMessage.Notification notification = remoteMessage.getNotification();
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+    private void sendNotification(final RemoteMessage remoteMessage) {
+        final RemoteMessage.Notification notification = remoteMessage.getNotification();
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(notification.getTitle())
                 .setContentText(notification.getBody())
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent);
+                .setAutoCancel(true);
 
-        NotificationManager notificationManager =
+        final NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
         notificationManager.notify(0, notificationBuilder.build());
     }
 }
