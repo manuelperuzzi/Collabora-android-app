acd282008043807ebf5f2da89c242f0562e041dd
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-09-12 11:42:02 +0200

    refactored alarm class

--- app/src/main/java/org/gammf/collabora_android/app/alarm/Alarm.java
+++ app/src/main/java/org/gammf/collabora_android/app/alarm/Alarm.java
@@ -6,9 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
-
 import org.joda.time.DateTime;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -16,9 +14,8 @@ import java.util.Locale;
 import static org.gammf.collabora_android.utils.AlarmAndGeofenceUtils.PREFS_NAME;
 
 /**
- * Created by Federico on 04/08/2017.
+ * Class that represent Allarm management
  */
-
 public class Alarm {
 
     /***
@@ -28,18 +25,11 @@ public class Alarm {
      * @param timeToSpawn exact time when spawn the notification
      */
     public void setAlarm(Context context, String message, DateTime timeToSpawn){
-
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(timeToSpawn.getYear(), timeToSpawn.getMonthOfYear(), timeToSpawn.getDayOfMonth(),
-              timeToSpawn.getHourOfDay(), timeToSpawn.getMinuteOfHour(), timeToSpawn.getSecondOfMinute());
-
         Intent intent = new Intent(context , AlarmBroadcastReceiver.class);
         intent.putExtra("title",message);
         intent.putExtra("time",timeToSpawn.getMillis());
-
         final long curretTime = System.currentTimeMillis();
-
         SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putLong(getDate(timeToSpawn.getMillis(), "dd/MM/yyyy hh:mm:ss"),curretTime);
@@ -61,17 +51,12 @@ public class Alarm {
      * @param timeToSpawn A simpler identifier for an alarm, TO TEST, maybe to change!!
      */
     public void deleteAlarm(Context context, DateTime timeToSpawn){
-
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context , AlarmBroadcastReceiver.class);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(timeToSpawn.getYear(), timeToSpawn.getMonthOfYear(), timeToSpawn.getDayOfMonth(),
-                timeToSpawn.getHourOfDay(), timeToSpawn.getMinuteOfHour(), timeToSpawn.getSecondOfMinute());
         SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         Log.d("DEBUG ID CANCEL", String.valueOf((int) prefs.getLong(getDate(timeToSpawn.getMillis(), "dd/MM/yyyy hh:mm:ss"),Long.MIN_VALUE)));
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, (int) prefs.getLong(getDate(timeToSpawn.getMillis(), "dd/MM/yyyy hh:mm:ss"),Long.MIN_VALUE),intent, PendingIntent.FLAG_UPDATE_CURRENT);
         am.cancel(pendingIntent);
-
     }
 
     /**
@@ -80,12 +65,8 @@ public class Alarm {
      * @param dateFormat Date format
      * @return String representing date in specified format
      */
-    public String getDate(long milliSeconds, String dateFormat)
-    {
-        // Create a DateFormatter object for displaying date in specified format.
+    private String getDate(long milliSeconds, String dateFormat){
         SimpleDateFormat formatter = new SimpleDateFormat(dateFormat, Locale.ITALIAN);
-
-        // Create a calendar object that will convert the date and time value in milliseconds to date.
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(milliSeconds);
         return formatter.format(calendar.getTime());
