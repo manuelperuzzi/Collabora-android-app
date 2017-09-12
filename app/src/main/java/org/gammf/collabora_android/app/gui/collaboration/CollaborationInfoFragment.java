b6960b3e5c79e546e2f04ab1f0a0cb8b5cdf03b3
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-09-12 18:22:03 +0200

    deleted unused imports

--- app/src/main/java/org/gammf/collabora_android/app/gui/collaboration/CollaborationInfoFragment.java
+++ app/src/main/java/org/gammf/collabora_android/app/gui/collaboration/CollaborationInfoFragment.java
@@ -2,7 +2,6 @@ package org.gammf.collabora_android.app.gui.collaboration;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
