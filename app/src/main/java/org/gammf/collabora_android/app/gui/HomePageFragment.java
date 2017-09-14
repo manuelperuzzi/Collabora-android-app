a7e07558b919abc2d30dccf5faff86de3c65474c
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-09-14 10:45:47 +0200

    created simple homepage gui + fragment

---
+++ app/src/main/java/org/gammf/collabora_android/app/gui/HomePageFragment.java
@@ -0,0 +1,46 @@
+package org.gammf.collabora_android.app.gui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import org.gammf.collabora_android.app.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomePageFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomePageFragment extends  Fragment {
+
+    public HomePageFragment() {
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment HomePageFragment.
+     */
+    public static HomePageFragment newInstance() {
+        return new HomePageFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_homepage, container, false);
+        initializeGuiComponent(rootView);
+        return rootView;
+    }
+
+    private void initializeGuiComponent(View rootView) {
+
+    }
+}
